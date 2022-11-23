e9ad3d0401020ea4cce4fae42ac9991140d42e61
caleb-freitas <calebfreitas@tutanota.com>
2022-11-23 18:31:10 -0300

    feat: added styles to sign-in page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,69 +1,78 @@
 import { type NextPage } from "next";
 import Link from "next/link";
+import Image from "next/image";
+
+import google from "../../assets/google.svg";
 
 const SignIn: NextPage = () => {
   return (
-    <>
-      <header>
-        <h1>Sign in</h1>
-        <p>New to scheduler?</p>
-        <Link href="/auth/sign-up">Sign up for an account.</Link>
-      </header>
-      <form>
-        <div className="mb-6">
-          <label
-            htmlFor="email"
-            className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Email address
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            placeholder="john.doe@company.com"
-            required
-          />
-        </div>
-        <div className="mb-6">
-          <label
-            htmlFor="password"
-            className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            placeholder="•••••••••"
-            required
-          />
-        </div>
-        <Link href="/auth/new-password">Forgot password?</Link>
-        <button
-          type="button"
-          className="mr-2 mb-2 rounded-lg bg-gray-900 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-        >
-          Sign in
-        </button>
-        <div>Or with</div>
-        <div>
+    <main className="mt-32 flex justify-center">
+      <div className="w-96">
+        <header className="pb-5">
+          <h2 className="text-2xl font-bold">Sign in</h2>
+          <div className="flex gap-1">
+            <p className="text-slate-600">New to scheduler?</p>
+            <Link className="text-blue-600" href="/auth/sign-up">
+              Sign up for an account.
+            </Link>
+          </div>
+        </header>
+        <form className="w-full">
+          <div className="mb-6">
+            <label
+              htmlFor="email"
+              className="mb-2 block text-sm font-bold text-slate-900"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="block w-full rounded-lg border border-slate-300 bg-slate-50 p-2.5 text-base text-slate-900 focus:bg-white"
+              placeholder="john.doe@company.com"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="mb-2 block text-sm font-bold text-slate-900 dark:text-white"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="block w-full rounded-lg border border-slate-300 bg-slate-50 p-2.5 text-sm text-slate-900 focus:bg-white"
+              placeholder="•••••••••"
+              required
+            />
+          </div>
+          <div className="flex flex-col">
+            <Link
+              className="mb-4 text-slate-600 transition-all hover:text-blue-600"
+              href="/auth/new-password"
+            >
+              Forgot password?
+            </Link>
+            <button
+              type="button"
+              className="mb-2 w-full rounded-lg bg-slate-800 px-5 py-2.5 text-base font-semibold text-white transition-all hover:bg-slate-900 focus:outline-none focus:ring-4 focus:ring-slate-300"
+            >
+              Sign in
+            </button>
+          </div>
+          <div className="my-4 text-center text-sm text-slate-600">Or with</div>
           <button
             type="button"
-            className="mr-2 mb-2 rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+            className="flex w-full items-center justify-center gap-2 rounded-lg border border-slate-300 bg-white px-5 py-2.5 text-base font-semibold text-slate-900 transition-all hover:bg-slate-100 focus:outline-none focus:ring-4 focus:ring-slate-200"
           >
-            X Google
+            <Image src={google} alt="google logo" width={20} />
+            Google
           </button>
-          <button
-            type="button"
-            className="mr-2 mb-2 rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-          >
-            X LinkedIn
-          </button>
-        </div>
-      </form>
-    </>
+        </form>
+      </div>
+    </main>
   );
 };
 
